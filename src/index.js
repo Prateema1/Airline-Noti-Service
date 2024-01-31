a675a68174737835efb1d442d6589b7dde655dc0
Prateem1 <prateema.adhikari@gmail.com>
2024-01-31 16:09:08 +0530

    created nodemailer transport object and tried mailing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 
-const { ServerConfig, Logger } = require('./config');
+const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+
+const mailSender = require('./config/email-config');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+
 app.use('/api', apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
  console.log(`Successfully started the server on PORT: ${ServerConfig.PORT}`);
- Logger.info("Successfully started the server", "root", {})
+ try {
+  const response = await mailSender.sendMail({
+    from: ServerConfig.GMAIL_EMAIL,
+    to: 'abc@gmail.com',
+    subject: "IS the service working ?",
+    text: 'Yes, it is working'
+   });
+ } catch(error) {
+  console.log(error);
+ }
 });
---
+++ src/utils/common/enums.js
@@ -0,0 +1,8 @@
+const EMAIL_STATUS_ENUMS = {
+  PENDING: 'pending',
+  SUCCESS: 'success',
+  FAILED: 'failed'
+}
+module.exports = {
+  EMAIL_STATUS_ENUMS
+}
